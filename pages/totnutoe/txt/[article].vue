83cfa7b9f270f42d443b88134eccaab0cc7b4ea0
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-06 21:42:21 +0200

    fixing article issue 2

---
+++ pages/totnutoe/txt/[article].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ContentQuery :path="`/articles/${getStringAfterLastSlash($route.path)}`" find="one" v-slot="{ data }">
+      <Article :article="data" />
+    </ContentQuery>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+layout: 'totnutoe',
+})
+
+function getStringAfterLastSlash(str) {
+  const regex = /[^/]+$/;
+  const match = str.match(regex);
+  return match ? match[0] : "";
+}
+
+</script>
