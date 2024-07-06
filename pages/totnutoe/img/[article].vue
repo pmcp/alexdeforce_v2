0fb2624a0c3275c9283563781840f6db4514dddc
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-07-06 21:40:45 +0200

    fixing article issue 1

--- pages/totnutoe/img/[article].vue
+++ pages/totnutoe/img/[article].vue
@@ -11,11 +11,8 @@ layout: 'totnutoe',
 })
 
 function getStringAfterLastSlash(str) {
-  // Match any characters after the last slash
   const regex = /[^/]+$/;
-  // Extract the matched portion using regex
   const match = str.match(regex);
-  // Return the matched portion
   return match ? match[0] : "";
 }
 
