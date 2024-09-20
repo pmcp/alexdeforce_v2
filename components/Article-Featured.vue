306427286ac9339e7112192cdfd0944c57cac95d
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-09-20 17:16:32 +0200

    removed image as featured because do it inside article

--- components/Article-Featured.vue
+++ components/Article-Featured.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="pb-5 pt-1"><span class="text-xl">{{ article.title }}</span></h1>
-    <nuxt-img v-if="article.image" :src="article.image" class="mb-4 w-full h-auto max-w-prose"/>
+<!--    <nuxt-img v-if="article.image" :src="article.image" class="mb-4 w-full h-auto max-w-prose"/>-->
     <ContentRenderer
       :value="article"
       class="text-sm prose"
